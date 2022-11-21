2b71e362c699cceacade8852445af31d4d5a4960
Martí T <marti.tarrason@enti.cat>
2022-11-21 08:34:18 +0100

    No se si funciona

--- client.sh
+++ client.sh
@@ -5,61 +5,76 @@ IP_LOCAL="127.0.0.1"
 
 PORT="4242"
 
-	echo "Cliente HMTP"
+echo "Cliente HMTP"
 
-	echo "(1) SEND - Enviando el Handshake"
+echo "(1) SEND - Enviando el Handshake"
 
-	echo "GREEN_POWA $IP_LOCAL" | nc $IP_SERVER $PORT
+MD5_IP=`echo $IP_LOCAL | md5sum | cut -d " " -f 1`
 
-	echo "(2) LISTEN - Escuchando confirmación"
+echo "GREEN_POWA $IP_LOCAL $MD5_IP" | nc $IP_SERVER $PORT
+
+echo "(2) LISTEN - Escuchando confirmación"
 
 MSG=`nc -l $PORT`
 
-	if [ "$MSG" != "OK_HMTP" ]
-		then
-			echo "ERROR 1: Handshake mal fomado"
-			exit 1
-		fi
+if [ "$MSG" != "OK_HMTP" ]
+then
+	echo "ERROR 1: Handshake mal fomado"
+	exit 1
+fi
 
-	echo "(5) SEND - Enviamos el nombre de archivo"
+echo "(5) SEND - Enviamos el nombre de archivo"
 
 FILE_NAME="elon_musk.jpg"
 
 FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
 
-	echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
 
-	echo "(6) LISTEN - Escuchando confirmación nombre archivo"
-
-MSG=`nc -l $PORT`
-
-	if [ "$MSG" != "OK_FILE_NAME" ]
-		then
-			echo "ERROR 2: Nombre de archivo enviado incorrectamente"
-			echo "	Mensaje de error: $MSG"
-
-			exit 2
-		fi
-
-	echo "(9) SEND - Enviamos datos del archivo"
-
-	cat memes/$FILE_NAME | nc $IP_SERVER $PORT
-
-	echo "(10) LISTEN - Escuchamos confirmación datos archivo"
+echo "(6) LISTEN - Escuchando confirmación nombre archivo"
 
 MSG=`nc -l $PORT`
 
-	if [ "$MSG" != "OK_DATA_RCPT" ]
-		then
-			echo "ERROR 3: Datos enviados incorrectamente"
-			exit 3
-		fi
+if [ "$MSG" != "OK_FILE_NAME" ]
+then
+	echo "ERROR 2: Nombre de archivo enviado incorrectamente"
+	echo "	Mensaje de error: $MSG"
 
-FILEHASH=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+	exit 2
+fi
 
-	echo "FILE_HASH $FILE_NAME $FILEHASH" | nc $IP_SERVER $PORT
+echo "(9) SEND - Enviamos datos del archivo"
+
+cat memes/$FILE_NAME | nc $IP_SERVER $PORT
+
+echo "(10) LISTEN - Escuchamos confirmación datos archivo"
+
 MSG=`nc -l $PORT`
 
-	echo "Fin del envío"
+if [ "$MSG" != "OK_DATA_RCPT" ]
+then
+	echo "ERROR 3: Datos enviados incorrectamente"
+	exit 3
+fi
 
-	exit 0
+
+echo "(13) SEND - MD5 de los datos"
+
+DATA_MD5=`cat memes/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "DATA_MD5 $DATA_MD5" | nc $IP_SERVER $PORT
+
+echo "(14) LISTEN - MD5 Comprobación"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_MD5" ]
+then
+	echo "ERROR 4: MD5 incorrecto"
+	echo "	Mensaje de error: $MSG"
+	exit 4
+fi
+
+echo "Fin del envío"
+
+exit 0
